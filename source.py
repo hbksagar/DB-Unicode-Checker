0a52fcf76321dbc26d56838110444ce840528a70
Krupa Sagar <sagar.krupahb2000@gmail.com>
2016-11-17 19:59:35 +0530

    Get non ascii characters from mysql database

---
+++ source.py
@@ -0,0 +1,76 @@
+from sqlalchemy import MetaData, create_engine, select, String, text
+from sqlalchemy.sql import table, literal_column
+import json
+
+# Provide read replica setting available in config or assign to None
+db_name = 'db_name'
+db_base = 'mysql://root:@localhost'
+db_config = '{0}/{1}'.format(db_base, db_name)
+
+# destination data type
+d_type = 'ascii'
+
+def build_condition(columns):
+    conditions = []
+    for column in columns:
+        conditions.append(
+            "{0} != CONVERT({0} USING {1})".format(column, d_type)
+        )
+    return " or ".join(conditions)
+
+def prepare_sql(table, columns):
+    return select([
+        text(columns)
+    ]).\
+    where(build_condition(tables[table])).\
+    select_from('{0}.{1}'.format(db_name, table))
+
+def non_ascii_data(table, columns):
+    sql = prepare_sql(table, ','.join(columns))
+    try:
+        result = engine.execute(sql)
+        keys = result.keys()
+        data_all =[]
+        for row in result:
+            data = {}
+            for index, key in enumerate(keys, start=0):
+                if row[index]:
+                    data[key] = row[index]
+            data_all.append(data)
+        return data_all
+    except Exception as e:
+        print e.message
+
+def non_ascii_data_per_table(tables):
+    table_data = {}
+    for table in tables:
+        data_all = non_ascii_data(table, tables[table])
+        if len(data_all):
+            table_data[table] = {}
+            table_data[table]['data'] = data_all
+            table_data[table]['count'] = len(data_all)
+        
+    return table_data
+
+def fetch_tables_metadata(m):
+    tables = {}
+    for table in m.tables.values():
+        string_cols = set()
+        for column in table.c:
+            if column.type.python_type is str:
+                string_cols.add(column.name)
+        if string_cols:
+            tables[table.name] = string_cols
+    return tables
+
+def get_engine(db_config):
+    return create_engine(
+        db_config,
+        convert_unicode=True
+    )
+
+engine = get_engine(db_config)
+m = MetaData()
+m.reflect(engine)
+tables = fetch_tables_metadata(m)
+print non_ascii_data_per_table(tables)
